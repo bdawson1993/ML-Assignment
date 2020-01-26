ffd5aaa8c3358e7da580f6a45190231bc811db10
Ben Dawson <17676646+bdawson1993@users.noreply.github.com>
2020-01-26 14:07:21 +0000

    update

--- train.py
+++ train.py
@@ -1,6 +1,19 @@
 import numpy as np
 import matplotlib.pyplot as plt
+
 from sklearn import neighbors
 from sklearn.metrics import accuracy_score
+from scipy import ndimage
+from scipy import misc
 
-print("Hello")
+##load image
+f = misc.face()
+
+
+classifier = neighbors.KNeighborsClassifier(3)
+classifier.fit(f[:,:], f[:,:1])
+
+predictions = classifier.predict(f[:,2])
+
+
+
