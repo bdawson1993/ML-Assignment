9558b5935ab3cdcd025a683013749e18fefaef80
Ben Dawson <17676646+bdawson1993@users.noreply.github.com>
2020-02-01 14:03:01 +0000

    file changes

---
+++ main.py
@@ -0,0 +1,12 @@
+import numpy as np
+import matplotlib.pyplot as plt
+
+
+
+from sklearn import neighbors
+from sklearn.metrics import accuracy_score
+
+pic = plt.imread("train\\cat.0.jpg")
+
+plt.imshow(pic)
+plt.show()
