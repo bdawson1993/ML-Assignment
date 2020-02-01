f3e72746d782e48e0bc36a0fe3078fa2b4f28ad5
Ben Dawson <17676646+bdawson1993@users.noreply.github.com>
2020-02-01 14:49:27 +0000

    added training data

--- main.py
+++ main.py
@@ -1,12 +1,19 @@
 import numpy as np
 import matplotlib.pyplot as plt
-
-
-
 from sklearn import neighbors
 from sklearn.metrics import accuracy_score
+from PIL import Image
 
-pic = plt.imread("train\\cat.0.jpg")
+img = Image.open("train/cat.0.jpg").convert("LA")
 
-plt.imshow(pic)
-plt.show()
+data = np.asarray(img)
+print(data.shape)
+
+classifiction = neighbors.KNeighborsClassifier(3)
+classifiction.fit(data[:,:,0], np.full(data[:,:,0].shape,1))
+
+predictions = classifiction.predict(data[:,:,0])
+
+print(predictions[:,:])
+
+
