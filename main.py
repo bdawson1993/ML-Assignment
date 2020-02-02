e91ce65c0f57c11d1b147c7b5d82f8bc9d04df5a
Ben Dawson <17676646+bdawson1993@users.noreply.github.com>
2020-02-02 17:10:28 +0000

    addition of a function and working model

--- main.py
+++ main.py
@@ -2,19 +2,39 @@ import numpy as np
 import matplotlib.pyplot as plt
 from sklearn import neighbors
 from sklearn.metrics import accuracy_score
-from PIL import Image
 from sklearn.datasets import load_iris
-
-iris = load_iris()
-
-print(iris)
-irisData = iris.data
-
-classfier = neighbors.KNeighborsClassifier(3)
-model = classfier.fit(iris.data, iris.target)
+import cv2
 
 
-print(irisData[1:2])
-print(model.predict(irisData[1:2]))
+#function to load image
+def LoadImg(tag, number):
+    img = cv2.imread("train/" + str(tag) + "." + str(number) + ".jpg")
+    img = cv2.resize(img, dsize=(100,100))
+    img = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+    return img
 
+
+
+img = LoadImg("cat", 50)
+img2 = LoadImg("dog", 51)
+
+newImg = np.concatenate((img, img2))
+
+target = np.full(img.shape[0], 0)
+target2 = np.concatenate((target, np.full(img2.shape[0], 1)))
+model = neighbors.KNeighborsClassifier(5)
+model.fit(newImg, target2)
+
+testImg = LoadImg("dog",12)
+
+
+
+predictions = model.predict(testImg)
+
+print(predictions)
+
+cv2.imshow("h", testImg)
+
+plt.imshow(newImg, cmap=plt.get_cmap("gray"))
+plt.show()
 
