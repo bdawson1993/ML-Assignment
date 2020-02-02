a0290502136f61db4d47dbf75e0dffd7c71498e5
Ben Dawson <17676646+bdawson1993@users.noreply.github.com>
2020-02-02 17:48:19 +0000

    Update main.py

--- main.py
+++ main.py
@@ -2,8 +2,8 @@ import numpy as np
 import matplotlib.pyplot as plt
 from sklearn import neighbors
 from sklearn.metrics import accuracy_score
-from sklearn.datasets import load_iris
 import cv2
+
 
 
 #function to load image
@@ -13,28 +13,49 @@ def LoadImg(tag, number):
     img = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
     return img
 
+#function to predict what the image holds
+def predict(model, data):
+    predDef = ["Cat", "Dog"]
+    predictions = model.predict(data)
+    counts = np.bincount(predictions)
+    print(counts)
+    if(counts[0] > counts[1]):
+        return predDef[0]
+    else:
+        return predDef[1]
+
+    return -1
+    
+
+
+
+
+
+
+
 
 
 img = LoadImg("cat", 50)
 img2 = LoadImg("dog", 51)
-
 newImg = np.concatenate((img, img2))
 
+#build up target array
 target = np.full(img.shape[0], 0)
 target2 = np.concatenate((target, np.full(img2.shape[0], 1)))
+
+#build model
 model = neighbors.KNeighborsClassifier(5)
 model.fit(newImg, target2)
 
-testImg = LoadImg("dog",12)
+
+
+#test data
+testImg = LoadImg("cat",50)
+x = predict(model, testImg)
+print(x)
 
 
 
-predictions = model.predict(testImg)
-
-print(predictions)
-
-cv2.imshow("h", testImg)
-
-plt.imshow(newImg, cmap=plt.get_cmap("gray"))
+plt.imshow(testImg, cmap=plt.get_cmap("gray"))
 plt.show()
 
